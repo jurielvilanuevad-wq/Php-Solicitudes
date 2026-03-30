fa02440e1a9a344d57dfa8bb1c435f8fb04a0c59
José Uriel Villanueva Donjuan <j.uriel.vilanueva.d@gmail.com>
2026-03-30 14:39:19 -0600

    Modificación de conexión.php y agregación de dockerfile

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,12 @@
 <?php
 
-// Conexión a la BD SQL de pruebas, 
-                    //  Server     Usuario  Contraseña   NombreBD
-$conexion=new mysqli("localhost",  "root",   "12345",   "mi_base");
+$conexion=new mysqli(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE')
+);
+
 $conexion->set_charset("utf8");
 
 ?>
