edea6ff8e8f2bc1c18121f75e511c2d64f94d05f
UrielVILd <j.uriel.vilanueva.d@gmail.com>
2026-05-13 01:06:27 -0600

    Cambio a variables de entorno

--- php/config_correo.php
+++ php/config_correo.php
@@ -2,9 +2,9 @@
 // Configuración SMTP para Gmail.
 // Genera la contraseña de aplicación en: https://myaccount.google.com/apppasswords
 // (requiere verificación en 2 pasos activada en tu cuenta Google)
-define('MAIL_HOST',      'smtp.gmail.com');
-define('MAIL_PORT',      587);
-define('MAIL_USERNAME',  'cardozacalderondaniel400@gmail.com');
-define('MAIL_PASSWORD',  'snqy ocgm tgtl lgig');
-define('MAIL_FROM',      'cardozacalderondaniel400@gmail.com');
-define('MAIL_FROM_NAME', 'ITSRV SOPORTEC');
+define('MAIL_HOST',      $_ENV['MAIL_HOST']);
+define('MAIL_PORT',      $_ENV['MAIL_PORT']);
+define('MAIL_USERNAME',  $_ENV['MAIL_USERNAME']);
+define('MAIL_PASSWORD',  $_ENV['MAIL_PASSWORD']);
+define('MAIL_FROM',      $_ENV['MAIL_FROM']);
+define('MAIL_FROM_NAME', $_ENV['MAIL_FROM_NAME']);
